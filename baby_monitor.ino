fa5e1bbf05def8af80d6c24c8a32a61da88dc5d9
MatanHai <matanhai25@gmail.com>
2025-08-26 17:45:15 +0300

    Create baby_monitor.ino

---
+++ baby_monitor.ino
@@ -0,0 +1,26 @@
+const int pirPin = 2;      // חיישן PIR
+const int ledPin = 3;      // נורת לד
+bool motionSent = false;   // נשלחה הודעה על תנועה?
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int pirState = digitalRead(pirPin);
+
+  if (pirState == HIGH && !motionSent) {
+    digitalWrite(ledPin, HIGH);               // הדלקת לד
+    Serial.println("MOTION_DETECTED");        // שליחת הודעה
+    motionSent = true;                        // סמן שנשלחה
+  }
+
+  if (pirState == LOW && motionSent) {
+    digitalWrite(ledPin, LOW);                // כיבוי לד
+    motionSent = false;                       // איפוס לדיווח הבא
+  }
+
+  delay(100); // שיהיה זמן "נשימה" ללולאה
+}
